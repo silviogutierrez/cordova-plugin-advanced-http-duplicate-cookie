7768993b4806c8f23e9ca067a0bc35ed27b00ad8
Silvio J. Gutierrez <silviogutierrez@gmail.com>
2022-12-27 16:44:08 -0500

    Cleanup and plaintext

--- shell.nix
+++ shell.nix
@@ -6,7 +6,15 @@ mkShell {
   buildInputs = requirements.production_dependencies
     ++ requirements.development_dependencies
     ++ requirements.contributing_dependencies ++ [
+      gradle
       cocoapods
       darwin.apple_sdk.frameworks.CoreServices
     ];
+  shellHook = ''
+    export JAVA_HOME="/Applications/Android Studio.app/Contents/jre/Contents/Home"
+    export ANDROID_HOME=~/Library/Android/sdk;
+    export PATH=$PATH:$ANDROID_HOME/tools;
+    export PATH=$PATH:$ANDROID_HOME/platform-tools;
+    export ANDROID_SDK_ROOT=~/Library/Android/sdk
+  '';
 }
